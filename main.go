97060201b513df193edd6e231c5cbe93c37ecfc5
bamcmanus <brent.mcmanus@me.com>
2025-10-31 09:40:25 -0700

    basic command execution

--- main.go
+++ main.go
@@ -4,20 +4,36 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"os/exec"
 	"strings"
 )
 
+func execInput(input string) error {
+	trimmed := strings.TrimSuffix(input, "\n")
+
+	cmd := exec.Command(trimmed)
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+
+	fmt.Printf("Command: \n%v\n", cmd)
+
+	return cmd.Run()
+}
+
 func main() {
 	for {
+		fmt.Print("> ")
+
 		reader := bufio.NewReader(os.Stdin)
-		str, err := reader.ReadString('\n')
+		input, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 		}
-		if strings.TrimSpace(str) == "exit" {
+		if strings.TrimSpace(input) == "exit" {
 			break
-		} else {
-			fmt.Printf("read string '%s'", str)
+		}
+		if err = execInput(input); err != nil {
+			fmt.Fprintln(os.Stderr, err)
 		}
 	}
 }
