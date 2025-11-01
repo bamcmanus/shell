b53f56cef5ea1009fb47054b5d16a865c9bbe5b3
bamcmanus <brent.mcmanus@me.com>
2025-11-01 10:42:42 -0700

    only construct command if it's needed

--- main.go
+++ main.go
@@ -17,13 +17,6 @@ func execInput(input, homeDir string) error {
 
 	parts := strings.Split(trimmed, " ")
 
-	cmd := exec.Command(parts[0], parts[1:]...)
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
-
-	if parts[0] == "cd" && len(parts) == 1 {
-		os.Chdir("/")
-	}
 	var err error
 	switch {
 	case parts[0] == "exit":
@@ -33,6 +26,9 @@ func execInput(input, homeDir string) error {
 	case parts[0] == "cd":
 		err = os.Chdir(parts[1])
 	default:
+		cmd := exec.Command(parts[0], parts[1:]...)
+		cmd.Stderr = os.Stderr
+		cmd.Stdout = os.Stdout
 		err = cmd.Run()
 	}
 
@@ -45,9 +41,8 @@ func outputPrompt(username, homeDir string) {
 		fmt.Fprintf(os.Stderr, "error getting working directory; using default prompt: %s\n", err)
 		fmt.Print(defaultPrompt)
 		return
-	} else {
-		fmt.Printf("Got working dir: %s\n", wd)
 	}
+
 	relativeWorkDir := strings.Replace(wd, homeDir, "~", 1)
 	if relativeWorkDir == "/" {
 		relativeWorkDir = "~"
